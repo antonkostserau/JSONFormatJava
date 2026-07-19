69f56c045cf800293129d28b7d04f2ef8752b1f5
Anton Kostserau <antonkostserau@gmail.com>
2026-07-19 21:28:34 +0300

    Added logic for JSONEntityObject.

--- src/main/java/io/github/antonkostserau/JSONFormatJava/JSONEntityObject.java
+++ src/main/java/io/github/antonkostserau/JSONFormatJava/JSONEntityObject.java
@@ -1,35 +1,51 @@
 package io.github.antonkostserau.JSONFormatJava;
 
 import java.util.Map;
+import java.util.HashMap;
+import java.util.StringJoiner;
 
 public class JSONEntityObject extends JSONEntity {
 
+    private final Map<String, JSONEntity> data;
+
     public JSONEntityObject() {
         super(JSONEntity.OBJECT);
+
+        this.data = new HashMap<String, JSONEntity>();
     }
 
     public JSONEntity get(String key) {
-        return null;
+        return this.data.get(key);
     }
 
     public JSONEntity put(String key, JSONEntity entity) {
-        return null;
+        return this.data.put(key, entity);
     }
 
     public JSONEntity remove(String key) {
-        return null;
+        return this.data.remove(key);
     }
-    
+
     public void clear() {
-        
+        this.data.clear();
     }
-    
+
     public int getCount() {
-        return -1;
+        return this.data.size();
     }
-    
+
     public Map<String, JSONEntity> toMap() {
-        return null;
+        return new HashMap<String, JSONEntity>(this.data);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(",", "{", "}");
+
+        for (Map.Entry<String, JSONEntity> entry : this.data.entrySet())
+            joiner.add("\"" + entry.getKey().replace("\\", "\\\\").replace("\"", "\\\"") + "\":" + entry.getValue());
+
+        return joiner.toString();
     }
 
 }
